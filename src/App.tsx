7fbe364bed31812474da2aaedabb42d420d404c6
juditperea <22jperea@ibadia.cat>
2026-08-20 12:01:00 +0200

    Initial project setup with React, TypeScript and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="main" className="min-h-screen bg-neutral-950 text-white flex items-center justify-center">
+        <h1 className="text-5xl font-bold">
+          Moodboard
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
